a4191abdb9b34a42853b8a365a95023a0f327fb4
Giuseppe Sanfrancesco <sanfra1407@live.it>
2019-01-23 00:40:21 +0100

    Created Cells molecule

---
+++ src/components/molecules/Cells.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cells">
+    <Cell v-for="(position, i) in positions" :key="i" :index="i" :position="position" />
+  </div>
+</template>
+
+<script>
+import { Cell } from '../atoms';
+
+export default {
+  name: 'Cells',
+  components: {
+    Cell
+  },
+  props: {
+    positions: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
---
+++ src/components/molecules/index.js
@@ -0,0 +1,5 @@
+import Cells from './Cells';
+
+export {
+  Cells,
+}
