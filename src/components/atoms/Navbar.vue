d6b7dce5a5748d9cb416d28ebec56797a5c21938
Giuseppe Sanfrancesco <sanfra1407@live.it>
2019-01-27 16:56:04 +0100

    Added transition effect to menu items

--- src/components/atoms/Navbar.vue
+++ src/components/atoms/Navbar.vue
@@ -4,9 +4,11 @@
       <router-link class="router-link" to="/">
         <span>Game</span>
       </router-link>
-      <router-link v-if="hasGameHistory" class="router-link" to="/history">
-        <span>Game history</span>
-      </router-link>
+      <transition name="fade">
+        <router-link v-if="hasGameHistory" class="router-link" to="/history">
+          <span>Game history</span>
+        </router-link>
+      </transition>
     </div>
   </div>
 </template>
