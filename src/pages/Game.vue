1e3e99e914f8c8335226a4eccd8d65183c92ceb1
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2019-01-30 23:25:25 +0100

    Add row to footer

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -3,25 +3,27 @@
     <Title>Game</Title>
     <Cells :positions="positions" />
     <Footer>
-      <div class="col">
-        <h3 class="footer__player">
-          <span v-if="winner">
-            <strong>{{ getWinnerName }}</strong> wins!
-          </span>
-          <span v-else-if="!winner && hasEmptyCells">{{ getPlayerName }} plays!</span>
-          <span v-else>Draw!</span>
-        </h3>
-      </div>
-      <div class="col">
-        <div class="text--right">
-          <transition name="bounce">
-            <button
-              class="btn btn--primary"
-              v-if="winner || !hasEmptyCells"
-              @click="$store.dispatch('newGame')">
-              <span>Play again</span>
-            </button>
-          </transition>
+      <div class="row">
+        <div class="col">
+          <h3 class="footer__player">
+            <span v-if="winner">
+              <strong>{{ getWinnerName }}</strong> wins!
+            </span>
+            <span v-else-if="!winner && hasEmptyCells">{{ getPlayerName }} plays!</span>
+            <span v-else>Draw!</span>
+          </h3>
+        </div>
+        <div class="col">
+          <div class="text--right">
+            <transition name="bounce">
+              <button
+                class="btn btn--primary"
+                v-if="winner || !hasEmptyCells"
+                @click="$store.dispatch('newGame')">
+                <span>Play again</span>
+              </button>
+            </transition>
+          </div>
         </div>
       </div>
     </Footer>
