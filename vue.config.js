0ff06f7d4a4f99dc1ab93b3cd1c9dd80e75a9ec6
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2020-03-04 23:14:17 +0100

    Update vue.config.js
    Add _getPublicPath and @pages alias

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : './'
-    : '/',
+const _getPublicPath = () => {
+  return process.env.NODE_ENV === 'production'
+    ? ( process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : './' )
+    : '/'
+}
 
-    configureWebpack: {
-      resolve: {
-        alias: {
-          "@atoms": path.resolve(__dirname, 'src/components/atoms/'),
-          "@molecules": path.resolve(__dirname, 'src/components/molecules/'),
-        }
+module.exports = {
+  publicPath: _getPublicPath(),
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@atoms": path.resolve(__dirname, 'src/components/atoms/'),
+        "@molecules": path.resolve(__dirname, 'src/components/molecules/'),
+        "@pages": path.resolve(__dirname, 'src/pages/'),
       }
     }
+  }
 }
