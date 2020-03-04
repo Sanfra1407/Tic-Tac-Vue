5bc2f7a75231dd1156f2c7f8c71106e23ce21a35
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2020-03-04 22:51:07 +0100

    Update vue.config.js
    Replace baseUrl with publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
     ? process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : './'
     : '/'
 }
