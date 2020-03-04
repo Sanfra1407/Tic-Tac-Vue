09d4ea7a567bfe320d47b457bbfe5801b8b86ad8
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2020-03-04 22:57:58 +0100

    Update main.js
    Code refactoring

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import TicTacVue from "./TicTacVue.vue";
 import router from "./router";
 import store from "./store";
+import TicTacVue from "./TicTacVue";
 
 Vue.config.productionTip = false;
 
