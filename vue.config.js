dbb6c365360c04f7eb3dd0b789e9ae9d2df40307
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2020-03-04 22:58:21 +0100

    Update vue.config.js
    Add @atoms and @molecules aliases

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const path = require('path');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE_URL : './'
-    : '/'
+    : '/',
+
+    configureWebpack: {
+      resolve: {
+        alias: {
+          "@atoms": path.resolve(__dirname, 'src/components/atoms/'),
+          "@molecules": path.resolve(__dirname, 'src/components/molecules/'),
+        }
+      }
+    }
 }
