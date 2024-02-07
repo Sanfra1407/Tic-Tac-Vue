06ba072e7dbb396ef2f21003bd255988ee242059
Giuseppe Sanfrancesco <sanfra1407@live.it>
2024-02-07 12:59:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
@@ -11,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  }
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3080,
+  },
 })
